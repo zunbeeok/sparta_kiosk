9d9f0c5a3c2465979f7637f8be5b01b9e2bfd29a
Seungsang Lee <ss.lee@dgraphcorp.com>
2023-12-13 16:40:54 +0900

    KimBap 출력 부분

---
+++ src/main/kotlin/menu/KimBap.kt
@@ -0,0 +1,16 @@
+package menu
+
+class KimBap {
+    fun addMenu() {
+        println("[ KimBap ]")
+        println("1. Origin KimBap   | 일반 김밥")
+        println("2. Tuna KimBap     | 참치와 마요네즈가 추가 된 김밥")
+        println("3. Anchovy KimBap  | 멸치가 추가 된 김밥")
+        println("4. Ki-to KimBap    | 키토 김밥")
+        println("0. 종료             | 뒤로가기")
+    }
+
+    fun getMenu() {
+
+    }
+}
