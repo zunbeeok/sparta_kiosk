6a2d74ceb6c9983e74aa0f87ade576be9f676b8e
zunbee <zunbeeok@gmail.com>
2023-12-13 16:19:33 +0900

    공통 order부분 추가

--- src/main/kotlin/order/Order.kt
+++ src/main/kotlin/order/Order.kt
@@ -4,7 +4,13 @@ package order
 //김밥, 분식, 식사, 음료
  class Order {
      fun getMenu(){
-
+        println("아래 메뉴판을 보시고 메뉴를 골라 입력해주세요.");
+         println(" [ 김밥천국 메뉴 ] ");
+         println(" 1. 김밥");
+         println(" 2. 분식");
+         println(" 3. 식사");
+         println(" 4. 음료");
+         println(" 0. 종료")
      };
 
     fun addCart(){
