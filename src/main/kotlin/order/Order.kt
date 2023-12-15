8fa228ed02c79aa169e2457e9414c553be247561
min <151513470+tkfkadldy@users.noreply.github.com>
2023-12-15 14:16:20 +0900

    Update Order.kt

--- src/main/kotlin/order/Order.kt
+++ src/main/kotlin/order/Order.kt
@@ -5,22 +5,19 @@ import food.Food
 
 //김밥, 분식, 식사, 음료
  interface Order {
-    // 주문할 음식 리스트.
-    val cartList:List<Food>;
+ // 주문 내역을 아래형태로 출력해준다.
+    // [ Orders ]
+    // 1. ShackBurger   |  1 개  | W 6.9
+    // 2. ChickenBurger |  2 개  | W 13.0   //치킨버거 하나당 가격은 6500원, 2개이므로 13.0을 출력.
+    fun printCartList(foodList:List<Food>):Unit;
 
-    // 메뉴 결정시 카트에 주문을 추가한다.
-    fun addCart(food: Food, count:Int):List<Food>;
+   //주문 금액의 총 가격을 아래 형태로 return해준다.
+   //   [ Total ]
+   //   W 6.9
+    fun printOrderToTalPrice(cart: Cart):Unit;
 
-    // getCartList에서 반환되는 index로 카트에 요소를 삭제한다.
-    fun deleteCart(index: Int):List<Food>;
+   //금액별로 포인트를 출력해주는 함수.
+    fun printPointResult(bank: Bank):Unit;
 
-    // cartList의 배열을 반환한다.
-    fun getCartList():List<Food>;
-
-    // cartList의 배열을 출력해준다.
-    fun printCartList();
-
-    // cartList의 총금액을 반환한다.
-    fun getTotalPrice():Int;
 
 }
